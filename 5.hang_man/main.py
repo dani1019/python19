411941800a883718992257d7b46a054bf41c4933
jooyong yang <nihonkita17@gmail.com>
2023-08-16 10:49:18 +0900

    update the game of hangman

--- 5.hang_man/main.py
+++ 5.hang_man/main.py
@@ -1,6 +1,5 @@
 import picture as pic
 import words as wd
-import method as md
 import random
 
 #start the hang_man
@@ -8,45 +7,38 @@ print(pic.hang_man)
 
 #select randomly answer
 answer = random.choice(wd.words).lower()
-print(answer)
 
-#guess the answer and enter the letter
-letter= input("Guess a letter: ").lower()
+#convert display_string to list of letters
+display_letter_list = list('-'* len(answer))
+#convert answer to list of list of letters
+answer_letter_list = list(answer)
+death = 0
 
-"""
-１．ユーザが入力したalphabetが表示された単語に含まれているか判断する
-　・どう判断するか
-　・ユーザーが入力したalaphabetが単語に存在する場合、画面に表示される単語と正解の単語を別々作成する
-　・入力するたびに画面に表示される単語にユーザーが入力したalphabetが反映されるようにする
-　→画面表示の単語をどこかに入れ、ユーザがalphabet入力処理に使われるようにする
-"""
-display_first_string =list(answer)
-#check if letter entered include in words
+#picture list
+pic = [pic.one, pic.two, pic.three, pic.four, pic.five]
+
 while True:
-    md.match_letter(display_first_string,letter)
+    #guess the answer and enter the letter
+    letter= input("Guess a letter: ").lower()
 
+    #if letter exist answer, update display_string
+    if letter in answer:
+      for index, answer_letter in enumerate(list(answer)):
+          if letter == answer_letter:
+              display_letter_list[index] = letter
+    #if letter not exist answer, life minus 1
+    else:
+        death += 1
+        if death == 6:
+            print("Game over")
+            break
+        else:
+          print(pic[death - 1])
 
-
-"""
-2.ユーザーが入力したalphabetが提示された単語に含まれている場合、
-　・ユーザーが入力したalphabetの単語の一部分として表示される
-  →単語のalphabetが表示されるとYou Winで終了。
-　
-　ユーザーが入力したalphabetが提示された単語に含まれていない場合、
-　・5回未満
-　　１。の処理が繰り返される
-    hang_manの絵が表示される
-  ・5回
-    Game overが画面に表示される
-"""
-
-
-display_string = []
-
-
-
-#if alphabet include in words and display the letter
-
-#if alphabet don't include in words 
-#You guesed a {alphabet}, that's not in the word. You lose a life
-#display the picture
+    #if display_string match answer, user win and exit game
+    if "".join(display_letter_list) == "".join(answer_letter_list):
+          print("You win.")
+          break
+    
+    #print display_string
+    print("".join(display_letter_list))
